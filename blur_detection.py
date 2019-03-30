756956992fa627052bffc4d5c9a452c2d485b69a
Nik Eastburn <nik@ndeast.com>
2019-03-30 17:30:40 -0400

    image size

--- blur_detection.py
+++ blur_detection.py
@@ -2,12 +2,15 @@ import cv2
 
 threshold = 100.0
 
-image = cv2.imread('images/ferarri.jpg')
-
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 def laplacian_variance(image):
     return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def image_size(image):
+    dimensions = image.shape
+    return image.shape[0] * image.shape[1]
+
+image = cv2.imread('images/ferrari800600.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 text = "Not Blurry"
 
@@ -18,3 +21,5 @@ if (fm < threshold):
 
 print(text)
 print(fm)
+print(image_size(image))
+
