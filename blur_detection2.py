8987f9ba2d6555c7dcd3e921498d375aca917285
Erik Flickinger <eflick98@gmail.com>
2019-03-30 17:44:49 -0400

    Update blur_detection2.py

--- blur_detection2.py
+++ blur_detection2.py
@@ -1,19 +1,34 @@
-import imutils
 import cv2
 import os
+
+threshold = 100.0
+
+def laplacian_variance(image):
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def image_size(image):
+    dimensions = image.shape
+    return image.shape[0] * image.shape[1]
+
+def print_values(images):
+    for image in images:
+        print(image[0])
+        print('Variance: ' + str(laplacian_variance(image[1])))
+        print('Size: ' + str(image_size(image[1])) + '\n')
+    return
+
 images = list()
 
-path = '/Users/Erik/Documents/GitHub/imagesort/TestImages'
+path = os.getcwd() + '\TestImages'
+
 for filename in os.listdir(path):
-    images.append(tuple((filename, cv2.imread(path + filename))))
+    images.append(tuple((filename, cv2.imread(path + '\\' + filename))))
 
-print(images)
+print_values(images)
 
-pic = cv2.imread(images[1][1])
-
-cv2.imshow('image', pic)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+# imshow(name, image)
+# cv2.imshow(images[0][0], images[0][1])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
